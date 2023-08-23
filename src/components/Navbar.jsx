d4d6241884d4c81c92e9a52ba8cb4cfa49920aa1
Your Name <you@example.com>
2023-08-23 12:03:59 +0530

    inital-commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+  const allusers = useSelector((state) => state.app.users);
+  const dispatch = useDispatch();
+
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid ">
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Create Post
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/read" className="nav-link">
+                  All Post ({allusers.length})
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
